df2fccb730b9a6618a304f3771b13a55fb72d91d
Ana Beatriz <anabiawalker@gmail.com>
2022-01-17 20:15:45 -0300

    Adicionando data, getters e setters

--- models/User.js
+++ models/User.js
@@ -3,14 +3,55 @@ class User{
     constructor(name, email, gender, birth, country, password, photo, admin){
         //this.atributo = variável do parâmetro
         //Inicializando os valores para serem usados em métodos dentro da classe
-        this.name = name;
-        this.gender = gender;
-        this.birth = birth;
-        this.country = country;
-        this.email = email;
-        this.password = password;
-        this.photo = photo;
-        this.admin = admin;
+        this._name = name;
+        this._gender = gender;
+        this._birth = birth;
+        this._country = country;
+        this._email = email;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
+        this._register = new Date()
     }
+    //Métodos que retornam a propriedade corretamente
+    get register(){
+        return this._register
+    }
+
+    get name(){
+        return this._name
+    }
+
+    get email(){
+        return this._email
+    }
+
+    get gender(){
+        return this.gender
+    }
+
+    get birth(){
+        return this._birth
+    }
+
+    get country(){
+        return this._country
+    }
+
+    get password(){
+        return this._password
+    }
+
+    get photo(){
+        return this._photo
+    }
+
+    get admin(){
+        return this._admin
+    } 
     
+    //Método de alterar a propriedade corretamente
+    set photo(value){
+        this._photo = value
+    }
 }
